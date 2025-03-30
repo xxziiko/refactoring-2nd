b62405a2d50ada65ea123975f6277ffc553f5ae6
xxziiko <xxziiko@gmail.com>
2025-03-30 18:54:07 +0900

    refactor: 반복문을 파이프라인으로 바꾸기

--- week 1/statement.ts
+++ week 1/statement.ts
@@ -1,52 +1,76 @@
 type PlayMeta = { name: string; type: string };
-type PlayID = "hamlet" | "as-like" | "othello";
-type Perfomance = { playID: PlayID; audience: number };
+export type PlayID = "hamlet" | "as-like" | "othello";
+export type Performance = { playID: PlayID; audience: number };
 
+type EnrichedPerformance = Performance & {
+  play: PlayMeta;
+  amount: number;
+  volumeCredits: number;
+};
+
+type StatementData = {
+  customer: string;
+  performances: EnrichedPerformance[];
+  totalAmount: number;
+  totalVolumeCredits: number;
+};
 export interface Plays {
   [key: string]: PlayMeta;
 }
 
 export interface Invoice {
-  performances: Perfomance[];
+  performances: Performance[];
   customer: string;
 }
 
-function statement(invoice: Invoice) {
-  return renderPlainText(invoice);
+export function statement(invoice: Invoice) {
+  const statementData: StatementData = {
+    customer: invoice.customer,
+    performances: invoice.performances.map(enrichPerformance),
+    totalAmount: 0,
+    totalVolumeCredits: 0,
+  };
+
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+
+  return renderPlainText(statementData);
 }
 
-function renderPlainText(invoice: Invoice) {
-  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+// object.assign 말고 스프레드 연산자 사용한 이유
+export function enrichPerformance(
+  aPerformance: Performance,
+): EnrichedPerformance {
+  return {
+    ...aPerformance,
+    play: playFor(aPerformance),
+    amount: amountFor(aPerformance),
+    volumeCredits: volumeCreditsFor(aPerformance),
+  };
+}
 
-  for (const perf of invoice.performances) {
+export function renderPlainText(statementData: StatementData) {
+  let result = `청구내역 (고객명: ${statementData.customer})\n`;
+
+  for (const perf of statementData.performances) {
     result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(totalAmount(statementData))}\n`;
+  result += `적립 포인트: ${totalVolumeCredits(statementData)}점\n`;
 
   return result;
 }
 
-function totalAmount() {
-  let result = 0;
-  for (const perf of invoice.performances) {
-    result += amountFor(perf);
-  }
-
-  return result;
+export function totalAmount(data: StatementData) {
+  return data.performances.reduce((total, p) => total + p.amount, 0);
 }
 
-function totalVolumeCredits() {
-  let result = 0;
-  for (const perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
-  }
-
-  return result;
+export function totalVolumeCredits(data: StatementData) {
+  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 }
 
-function usd(aNumber: number) {
+export function usd(aNumber: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
@@ -54,7 +78,7 @@ function usd(aNumber: number) {
   }).format(aNumber / 100);
 }
 
-function volumeCreditsFor(perf: Perfomance) {
+export function volumeCreditsFor(perf: Performance) {
   let result = 0;
 
   result += Math.max(perf.audience - 30, 0);
@@ -64,11 +88,11 @@ function volumeCreditsFor(perf: Perfomance) {
   return result;
 }
 
-function playFor(aPerfomance: Perfomance) {
+export function playFor(aPerfomance: Performance) {
   return plays[aPerfomance.playID];
 }
 
-function amountFor(aPreformance: Perfomance) {
+export function amountFor(aPreformance: Performance) {
   let result = 0;
   const play = playFor(aPreformance);
 
@@ -109,5 +133,3 @@ const invoice: Invoice = {
 };
 
 console.log(statement(invoice));
-
-export default statement;
