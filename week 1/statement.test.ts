3679b80a62a312e859a002acf8683dd50744bf67
xxziiko <xxziiko@gmail.com>
2025-03-30 19:03:46 +0900

    test: 빠진 인자 추가

--- week 1/statement.test.ts
+++ week 1/statement.test.ts
@@ -1,5 +1,5 @@
 // statement.test.ts
-import { type Invoice, statement } from "./statement";
+import { type Invoice, type Plays, statement } from "./statement";
 
 describe("statement()", () => {
   it("고객 이름과 각 공연, 총액, 포인트를 포함한 청구서 문자열을 반환해야 함", () => {
@@ -10,8 +10,13 @@ describe("statement()", () => {
         { playID: "othello", audience: 35 },
       ],
     };
+    const plays: Plays = {
+      hamlet: { name: "Hamlet", type: "tragedy" },
+      "as-like": { name: "As You Like It", type: "comedy" },
+      othello: { name: "Othello", type: "tragedy" },
+    };
 
-    const result = statement(invoice);
+    const result = statement(invoice, plays);
     const expectedLines = [
       "청구내역 (고객명: BigCo)",
       "Hamlet : $650.00 (55석)",
