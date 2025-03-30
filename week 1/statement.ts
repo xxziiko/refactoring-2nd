6042c5fe78af56dfcd12e8e7261cac2698198f90
xxziiko <xxziiko@gmail.com>
2025-03-30 22:50:08 +0900

    refactor: renderHtml

--- week 1/statement.ts
+++ week 1/statement.ts
@@ -41,6 +41,26 @@ export function statement(invoice: Invoice, plays: Plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+export function htmlStatement(invoice: Invoice, plays: Plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+export function renderHtml(data: StatementData): string {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += `<table>\n`;
+  result += `<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n`;
+
+  for (const perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}석</td><td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += `</table>\n`;
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+  return result;
+}
+
 export function createStatementData(invoice: Invoice, plays: Plays) {
   const result: StatementData = {
     customer: invoice.customer,
