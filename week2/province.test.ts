790357557f9f1dda8c5d36db7cfbb67086a5f7cf
xxziiko <xxziiko@gmail.com>
2025-04-08 19:44:53 +0900

    test: 두번째 테스트

--- week2/province.test.ts
+++ week2/province.test.ts
@@ -8,4 +8,9 @@ describe("Province", () => {
     const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5);
   });
+
+  it("profit", () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).toEqual(230);
+  });
 });
