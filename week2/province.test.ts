54a779256bb68f1670f30f3346aa8e290f75b40e
xxziiko <xxziiko@gmail.com>
2025-04-08 20:18:50 +0900

    test: 픽스처 수정하기

--- week2/province.test.ts
+++ week2/province.test.ts
@@ -16,4 +16,13 @@ describe("Province", () => {
   it("profit", () => {
     expect(asia.profit).toEqual(230);
   });
+
+  // 설정-실행-검증 패턴
+  // 일반적으로 it 구문 하나당 검증도 하나씩 하는게 좋다. (실패 시 원인 파악 쉬움)
+  // 이 테스트는 두 가지 검증을 한 번에 하고 있음. (한 테스트로 묶어도 문제되지 않을 정도로 두 속성이 밀접하다고 판단)
+  it("change production", () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toEqual(-6);
+    expect(asia.profit).toEqual(292);
+  });
 });
