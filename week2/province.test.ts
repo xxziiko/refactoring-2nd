61b79fb60600ed896646b6fc3139fb0779def3ed
xxziiko <xxziiko@gmail.com>
2025-04-08 19:35:05 +0900

    test: 첫번째 테스트

---
+++ week2/province.test.ts
@@ -0,0 +1,11 @@
+import { Province, sampleProvinceData } from "./Province";
+
+// 1. 픽스처(province) 생성
+describe("Province", () => {
+  // 2. 픽스처의 속성을 검증한다.
+
+  it("shortFall", () => {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.shortfall).toEqual(5);
+  });
+});
