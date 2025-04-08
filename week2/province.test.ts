879dc22a885236324bd4b48d893c4d78a28b7953
xxziiko <xxziiko@gmail.com>
2025-04-08 23:47:04 +0900

    test: 경계조건 검사하기

--- week2/province.test.ts
+++ week2/province.test.ts
@@ -25,4 +25,57 @@ describe("Province", () => {
     expect(asia.shortfall).toEqual(-6);
     expect(asia.profit).toEqual(292);
   });
+
+  // 경계 조건 테스트 (0, 음수 등)
+  it("zero demand", () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toEqual(-25);
+    expect(asia.profit).toEqual(0);
+  });
+
+  it("negative demand", () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toEqual(-26);
+    expect(asia.profit).toEqual(-10);
+  });
+
+  // 의미상 숫자만 입력 받아야 하지만 문자열을 취해야 하는 경우
+  it("empty string demand", () => {
+    asia.demand = "";
+    expect(asia.shortfall).toBeNaN();
+    expect(asia.profit).toBeNaN();
+  });
+});
+
+describe("no producers", () => {
+  let noProducers: Province;
+
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+
+  it("shortfall", () => {
+    expect(noProducers.shortfall).toEqual(30);
+  });
+
+  it("profit", () => {
+    expect(noProducers.profit).toEqual(0);
+  });
+});
+
+describe("string for producers", () => {
+  it("string for producers", () => {
+    const data = {
+      name: "String producers",
+      producers: ["Producer"],
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toEqual(30);
+  });
 });
