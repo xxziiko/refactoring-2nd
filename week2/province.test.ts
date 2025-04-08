05af662de53e6cc87357384bcfffe53cac5d2431
xxziiko <xxziiko@gmail.com>
2025-04-08 20:08:21 +0900

    test: 매번 픽스처 생성하기

--- week2/province.test.ts
+++ week2/province.test.ts
@@ -2,15 +2,18 @@ import { Province, sampleProvinceData } from "./Province";
 
 // 1. 픽스처(province) 생성
 describe("Province", () => {
-  // 2. 픽스처의 속성을 검증한다.
+  let asia: Province;
 
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
+
+  // 2. 픽스처의 속성을 검증한다.
   it("shortFall", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toEqual(5);
   });
 
   it("profit", () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toEqual(230);
   });
 });
